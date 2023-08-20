69cf2eaf067f186ea1e6e60904bd301c4a9520e0
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-21 00:00:37 +0530

    Fixed Error for the border img

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import {
   SuperQuality,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">

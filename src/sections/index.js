2e09400c3c6a77b25f6057fdc2be60cbb5435e57
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-08-20 17:58:39 +0530

    Inital Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProduct';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export  {
+    Hero,
+    PopularProducts,
+    Services,
+    CustomerReviews,
+    Footer,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
